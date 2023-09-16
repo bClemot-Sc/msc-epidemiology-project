2f1f4fa8f60b75353231c93784a6befe442579d4
paquittet <105439564+paquittet@users.noreply.github.com>
2023-09-16 14:01:26 +0200

    Update MEPI_PROJET_SCRIPT.r

--- MEPI_PROJET_SCRIPT.r
+++ MEPI_PROJET_SCRIPT.r
@@ -1,29 +1,29 @@
-### Modèle dont la sensibilité doit être analysée dans le cadre du projet MODE-MPI 2023-2024
+### Mod?le dont la sensibilit? doit ?tre analys?e dans le cadre du projet MODE-MPI 2023-2024
 
-### Le modèle est ici défini sous forme de fonction pour faciliter vos analyses de sensibilité (AS)
-### La fonction renvoie les sorties ponctuelles qui sont à analyser dans l'AS
+### Le mod?le est ici d?fini sous forme de fonction pour faciliter vos analyses de sensibilit? (AS)
+### La fonction renvoie les sorties ponctuelles qui sont ? analyser dans l'AS
 
 modAppli <- function(parametre){  
 
   # CONDITIONS DE SIMULATION
   temps = 2*365; # nb de pas de temps (en jours)
-  # initialisation pour la sauvegarde de 4 sorties ponctuelles pour chaque jeu de paramètres
+  # initialisation pour la sauvegarde de 4 sorties ponctuelles pour chaque jeu de param?tres
   sorties <- matrix(0, nrow=nrow(parametre), ncol=4)
 
-  # boucle des scénarios de l'échantillonnage de l'AS
+  # boucle des sc?narios de l'?chantillonnage de l'AS
   for (i in 1:nrow(parametre)) { 
 
     # STRUCTURE & PARAMETRES DU MODELE
 
     # XX
-    K = parametre[i,1];		# xx
+    K = parametre[i,1];		# CapacitÃ© d'acceuil
     sr = parametre[i,2];	# xx
     m1 = parametre[i,3];	# xx
     m2 = parametre[i,4];	# xx
     m3 = parametre[i,5];	# xx
     f2 = parametre[i,6];	# xx
     f3 = parametre[i,7];	# xx
-    portee = parametre[i,8];	# xx
+    portee = parametre[i,8];	# Nb d'enfant
     t1 = parametre[i,9];	# xx
     t2 = parametre[i,10];	# xx
 
@@ -35,43 +35,43 @@ modAppli <- function(parametre){
     madd = parametre[i,15];	# xx
 
     # INITIALISATION
-    MAT <- array(0, dim=c(4,4,temps)); # nb indiv par classe d'âge en ligne (dernière ligne = pop tot), état de santé en colonne, pas de temps (dimension 3)
+    MAT <- array(0, dim=c(4,4,temps)); # nb indiv par classe d'?ge en ligne (derni?re ligne = pop tot), ?tat de sant? en colonne, pas de temps (dimension 3)
     nvinf <- array(0, dim=c(temps));
-    # conditions initiales (la population est à sa structure d'équilibre, calculée par ailleurs)
+    # conditions initiales (la population est ? sa structure d'?quilibre, calcul?e par ailleurs)
     MAT[1,1,1] <- 27; # xx
     MAT[2,1,1] <- 23; # xx
     MAT[3,1,1] <- 36; # xx
     MAT[3,3,1] <- 1;  # xx
-    # effectifs par état de santé
+    # effectifs par ?tat de sant?
     MAT[4,1,1] <- sum(MAT[1:3,1,1]); MAT[4,2,1] <- sum(MAT[1:3,2,1]); MAT[4,3,1] <- sum(MAT[1:3,3,1]); MAT[4,4,1] <- sum(MAT[1:3,4,1]);
 
     # SIMULATIONS
     # boucle du temps
     for (t in 1:(temps-1)) { 
-     # classe d'âge xx
-      # RQ : les naissances sont XX, les nouveaux nés étant dans l'état XX
+     # classe d'?ge xx
+      # RQ : les naissances sont XX, les nouveaux n?s ?tant dans l'?tat XX
       N <- sum(MAT[4,,t]);	# taille de la pop en t
 	MAT[1,1,t+1] <- MAT[1,1,t]*(1-m1-t1-trans*MAT[4,3,t]/N) + loss*MAT[1,4,t]      + max(0, sr*portee*(sum(MAT[2,,t])*f2 + sum(MAT[3,,t])*f3) * (1 - N/K)); 
 	MAT[1,2,t+1] <- MAT[1,2,t]*(1-m1-t1-lat)			  + trans*MAT[1,1,t]*MAT[4,3,t]/N; 
 	MAT[1,3,t+1] <- MAT[1,3,t]*(1-m1-madd-t1-rec)  		  + lat*MAT[1,2,t]; 
 	MAT[1,4,t+1] <- MAT[1,4,t]*(1-m1-t1-loss) 		  + rec*MAT[1,3,t]; 
-     # classe d'âge xx
+     # classe d'?ge xx
 	MAT[2,1,t+1] <- MAT[1,1,t]*t1	+ MAT[2,1,t]*(1-m2-t2-trans*MAT[4,3,t]/N) + loss*MAT[2,4,t];
 	MAT[2,2,t+1] <- MAT[1,2,t]*t1	+ MAT[2,2,t]*(1-m2-t2-lat)			+ trans*MAT[2,1,t]*MAT[4,3,t]/N;
 	MAT[2,3,t+1] <- MAT[1,3,t]*t1	+ MAT[2,3,t]*(1-m2-madd-t2-rec)		+ lat*MAT[2,2,t];
 	MAT[2,4,t+1] <- MAT[1,4,t]*t1	+ MAT[2,4,t]*(1-m2-t2-loss)			+ rec*MAT[2,3,t];
-     # classe d'âge xx
+     # classe d'?ge xx
 	MAT[3,1,t+1] <- MAT[2,1,t]*t2	+ MAT[3,1,t]*(1-m3-trans*MAT[4,3,t]/N) 	+ loss*MAT[3,4,t];
 	MAT[3,2,t+1] <- MAT[2,2,t]*t2	+ MAT[3,2,t]*(1-m3-lat)				+ trans*MAT[3,1,t]*MAT[4,3,t]/N;
 	MAT[3,3,t+1] <- MAT[2,3,t]*t2	+ MAT[3,3,t]*(1-m3-madd-rec)			+ lat*MAT[3,2,t];
 	MAT[3,4,t+1] <- MAT[2,4,t]*t2	+ MAT[3,4,t]*(1-m3-loss)			+ rec*MAT[3,3,t];
-     # calcul des effectifs par état de santé
+     # calcul des effectifs par ?tat de sant?
 	MAT[4,1,t+1] <- sum(MAT[1:3,1,t+1]); MAT[4,2,t+1] <- sum(MAT[1:3,2,t+1]); MAT[4,3,t+1] <- sum(MAT[1:3,3,t+1]); MAT[4,4,t+1] <- sum(MAT[1:3,4,t+1]);
 	nvinf[t+1]   <- trans*MAT[4,1,t]*MAT[4,3,t]/N
 
     }# fin boucle temps
 
-    # sorties ponctuelles à analyser
+    # sorties ponctuelles ? analyser
     # XX
     sortie1 <- (MAT[4,2,temps]+MAT[4,3,temps])/sum(MAT[4,,temps])
     # xx
@@ -86,8 +86,8 @@ modAppli <- function(parametre){
     sorties[i,3] <- sortie3;
     sorties[i,4] <- sortie4;
     
-  }# fin boucle scénarios AS
+  }# fin boucle sc?narios AS
   return(sorties)
-} # fin fonction du modèle
+} # fin fonction du mod?le
 
 # END
